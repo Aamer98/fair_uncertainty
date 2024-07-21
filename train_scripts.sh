e0b8faf8c2c28b59146211f748cdf51a975bf3c9
aamer98 <ar.aamer@gmail.com>
2024-07-21 17:48:32 +0000

    local

---
+++ train_scripts.sh
@@ -0,0 +1,1 @@
+python -m subpopbench.train --algorithm ERM --dataset CheXpertNoFinding --train_attr no --data_dir /home/as26840@ens.ad.etsmtl.ca/data/subpopbench --output_dir /home/as26840@ens.ad.etsmtl.ca/repos/fair_uncertainty/logs --output_folder_name chexpert
