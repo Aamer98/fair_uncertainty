878438dff25b2ff2f4c3ad5101282d2013b3c5af
AS26840 <as26840@ens.ad.etsmtl.ca@lrl-lozen.ens.ad.etsmtl.ca>
2024-09-05 15:17:13 +0000

    uncertainty

--- subpopbench/utils/eval_helper.py
+++ subpopbench/utils/eval_helper.py
@@ -8,6 +8,20 @@ import netcal.metrics
 import ttach as tta # test time augmentation package
 
 
+# to get the samples from the loader
+def get_samples(loader):
+    num_labels = loader.dataset.num_labels
+
+    ys, atts, gs = [], [], []
+
+    for _, x, y, a in loader:
+        ys.append(y)
+        atts.append(a)
+        gs.append([f'y={yi},a={gi}' for c, (yi, gi) in enumerate(zip(y, a))])
+
+    return np.concatenate(ys, axis=0), np.concatenate(atts, axis=0), np.concatenate(gs)    
+
+
 def predict_on_set(algorithm, loader, device):
     num_labels = loader.dataset.num_labels
 
@@ -90,9 +104,242 @@ def TTA_eval(algorithm, loader, device):
     return np.concatenate(ps, axis=0)
 
 
-def test_metrics(algorithm, loader, device, thres=0.5):
-    # tta uncertainty
-    # preds = TTA_eval(algorithm, loader, device)
+def test_TTA(algorithm, loader, train_loader, device):
+
+    # Get train samples
+    train_targets, train_attributes, train_gs = get_samples(train_loader)
+
+    output_list = []
+    
+    breakpoint()
+
+    transforms = tta.Compose(
+        [
+            tta.HorizontalFlip(),
+            tta.Rotate90(angles=[0, 180]),
+            tta.Multiply(factors=[0.9, 1, 1.1]),        
+        ]
+    )
+
+    for i in range(dropout_iters):
+        targets, attributes, preds, gs = predict_on_set(algorithm, loader, device) # gs: group sensitive attribute: (target, attribute) pairing?
+        output_list.append(torch.from_numpy(preds))
+
+    # Calculate model batch statistics
+    output_list = [x.unsqueeze(-1).unsqueeze(-1) for x in output_list]
+    output_list = torch.cat(output_list,-1)
+    output_list = output_list.reshape(output_list.shape[-1], output_list.shape[0], output_list.shape[1]) # dim: (Dropout iters, total samples, output dim)
+    output_mean = output_list.mean(dim=0) # Confidence
+    output_variance = output_list.var(dim=0).mean().item() # Uncertainty
+
+    # Calculate averaged mc dropout output
+    preds = output_list.mean(dim=0).squeeze().numpy()
+    preds_rounded = preds >= thres if preds.squeeze().ndim == 1 else preds.argmax(1)
+    label_set = np.unique(targets)
+
+    # Calculate metrics
+    res = {}
+    res['per_attribute'] = {}
+    res['per_class'] = {} # TODO
+    res['per_group'] = {}
+
+    ## Overall metrics
+    res['overall'] = {
+        **binary_metrics(targets, preds_rounded, label_set),
+        **prob_metrics(targets, preds, label_set)
+    }
+
+    ## Per attribute metrics
+    for a in np.unique(attributes):
+        mask = attributes == a
+        res['per_attribute'][str(a)] = {
+            **binary_metrics(targets[mask], preds_rounded[mask], label_set),
+            **prob_metrics(targets[mask], preds[mask], label_set)
+        }
+        train_mask = train_attributes == a
+        res['per_attribute'][str(a)]['train_n_samples'] = len(train_targets[train_mask])
+
+    ## Per class metrics
+    classes_report = classification_report(targets, preds_rounded, output_dict=True, zero_division=0.)
+    res['overall']['macro_avg'] = classes_report['macro avg']
+    res['overall']['weighted_avg'] = classes_report['weighted avg']
+    for y in np.unique(targets):
+        res['per_class'][str(y)] = classes_report[str(y)]
+
+    for c in np.unique(targets):
+        mask = targets == c
+        res['per_class'][f'class_{str(c)}'] = {
+            **binary_metrics(targets[mask], preds_rounded[mask], label_set),
+            **attribute_metrics(targets[mask], preds[mask], label_set)
+        }
+        train_mask = train_targets == c
+        res['per_class'][f'class_{str(c)}']['train_n_samples'] = len(train_targets[train_mask])
+
+
+    ## Per group metrics
+    for g in np.unique(gs):
+        mask = gs == g
+        res['per_group'][str(g)] = {
+            **binary_metrics(targets[mask], preds_rounded[mask], label_set),
+            **attribute_metrics(targets[mask], preds[mask], label_set)
+        }
+        train_mask = train_gs == g
+        res['per_group'][str(g)]['train_n_samples'] = len(train_targets[train_mask])
+
+
+    res['adjusted_accuracy'] = sum([res['per_group'][str(g)]['accuracy'] for g in np.unique(gs)]) / len(np.unique(gs))
+    res['min_attr']  = pd.DataFrame(res['per_attribute']).min(axis=1).to_dict()
+    res['max_attr']  = pd.DataFrame(res['per_attribute']).max(axis=1).to_dict()
+    res['min_group'] = pd.DataFrame(res['per_group']).min(axis=1).to_dict()
+    res['max_group'] = pd.DataFrame(res['per_group']).max(axis=1).to_dict()
+
+
+    # Uncertainty metrics
+
+    ## Overall uncertainty
+    res['overall']['mean'] = output_list.mean().item()
+    res['overall']['variance'] = output_list.var(dim=0).mean().item()
+
+    ## Per attribute uncertainty
+    for a in np.unique(attributes):
+        mask = attributes == a
+        output_sublist = output_list[:, mask, :]
+        res['per_attribute'][str(a)]['mean'] = output_sublist.mean().item()
+        res['per_attribute'][str(a)]['variance'] = output_sublist.var(dim=0).mean().item()
+
+    # Per subgroup uncertainty
+    for g in np.unique(gs):
+        mask = gs == g
+        output_sublist = output_list[:, mask, :]
+        res['per_group'][str(g)]['mean'] = output_sublist.mean().item() 
+        res['per_group'][str(g)]['variance'] = output_sublist.var(dim=0).mean().item()
+
+    # Per class uncertainty TODO
+    for c in np.unique(targets):
+        mask = targets == c
+        output_sublist = output_list[:, mask, :]
+        res['per_class'][f'class_{str(c)}']['mean'] = output_sublist.mean().item()
+        res['per_class'][f'class_{str(c)}']['variance'] = output_sublist.var(dim=0).mean().item()
+
+    return res
+
+
+def test_mcdropout(algorithm, loader, train_loader, device, thres=0.5, dropout_iters=[1,5]):
+
+    # Get train samples
+    train_targets, train_attributes, train_gs = get_samples(train_loader)
+
+    output_list = []
+
+    # Run dropout iterations
+    algorithm.train() # enable dropout
+    for i in range(dropout_iters):
+        targets, attributes, preds, gs = predict_on_set(algorithm, loader, device) # gs: group sensitive attribute: (target, attribute) pairing?
+        output_list.append(torch.from_numpy(preds))
+
+    # Calculate model batch statistics
+    output_list = [x.unsqueeze(-1).unsqueeze(-1) for x in output_list]
+    output_list = torch.cat(output_list,-1)
+    output_list = output_list.reshape(output_list.shape[-1], output_list.shape[0], output_list.shape[1]) # dim: (Dropout iters, total samples, output dim)
+    output_mean = output_list.mean(dim=0) # Confidence
+    output_variance = output_list.var(dim=0).mean().item() # Uncertainty
+
+    # Calculate averaged mc dropout output
+    preds = output_list.mean(dim=0).squeeze().numpy()
+    preds_rounded = preds >= thres if preds.squeeze().ndim == 1 else preds.argmax(1)
+    label_set = np.unique(targets)
+
+    # Calculate metrics
+    res = {}
+    res['per_attribute'] = {}
+    res['per_class'] = {} # TODO
+    res['per_group'] = {}
+
+    ## Overall metrics
+    res['overall'] = {
+        **binary_metrics(targets, preds_rounded, label_set),
+        **prob_metrics(targets, preds, label_set)
+    }
+
+    ## Per attribute metrics
+    for a in np.unique(attributes):
+        mask = attributes == a
+        res['per_attribute'][str(a)] = {
+            **binary_metrics(targets[mask], preds_rounded[mask], label_set),
+            **prob_metrics(targets[mask], preds[mask], label_set)
+        }
+        train_mask = train_attributes == a
+        res['per_attribute'][str(a)]['train_n_samples'] = len(train_targets[train_mask])
+
+    ## Per class metrics
+    classes_report = classification_report(targets, preds_rounded, output_dict=True, zero_division=0.)
+    res['overall']['macro_avg'] = classes_report['macro avg']
+    res['overall']['weighted_avg'] = classes_report['weighted avg']
+    for y in np.unique(targets):
+        res['per_class'][str(y)] = classes_report[str(y)]
+
+    for c in np.unique(targets):
+        mask = targets == c
+        res['per_class'][f'class_{str(c)}'] = {
+            **binary_metrics(targets[mask], preds_rounded[mask], label_set),
+            **attribute_metrics(targets[mask], preds[mask], label_set)
+        }
+        train_mask = train_targets == c
+        res['per_class'][f'class_{str(c)}']['train_n_samples'] = len(train_targets[train_mask])
+
+
+    ## Per group metrics
+    for g in np.unique(gs):
+        mask = gs == g
+        res['per_group'][str(g)] = {
+            **binary_metrics(targets[mask], preds_rounded[mask], label_set),
+            **attribute_metrics(targets[mask], preds[mask], label_set)
+        }
+        train_mask = train_gs == g
+        res['per_group'][str(g)]['train_n_samples'] = len(train_targets[train_mask])
+
+
+    res['adjusted_accuracy'] = sum([res['per_group'][str(g)]['accuracy'] for g in np.unique(gs)]) / len(np.unique(gs))
+    res['min_attr']  = pd.DataFrame(res['per_attribute']).min(axis=1).to_dict()
+    res['max_attr']  = pd.DataFrame(res['per_attribute']).max(axis=1).to_dict()
+    res['min_group'] = pd.DataFrame(res['per_group']).min(axis=1).to_dict()
+    res['max_group'] = pd.DataFrame(res['per_group']).max(axis=1).to_dict()
+
+
+    # Uncertainty metrics
+
+    ## Overall uncertainty
+    res['overall']['mean'] = output_list.mean().item()
+    res['overall']['variance'] = output_list.var(dim=0).mean().item()
+
+    ## Per attribute uncertainty
+    for a in np.unique(attributes):
+        mask = attributes == a
+        output_sublist = output_list[:, mask, :]
+        res['per_attribute'][str(a)]['mean'] = output_sublist.mean().item()
+        res['per_attribute'][str(a)]['variance'] = output_sublist.var(dim=0).mean().item()
+
+    # Per subgroup uncertainty
+    for g in np.unique(gs):
+        mask = gs == g
+        output_sublist = output_list[:, mask, :]
+        res['per_group'][str(g)]['mean'] = output_sublist.mean().item() 
+        res['per_group'][str(g)]['variance'] = output_sublist.var(dim=0).mean().item()
+
+    # Per class uncertainty TODO
+    for c in np.unique(targets):
+        mask = targets == c
+        output_sublist = output_list[:, mask, :]
+        res['per_class'][f'class_{str(c)}']['mean'] = output_sublist.mean().item()
+        res['per_class'][f'class_{str(c)}']['variance'] = output_sublist.var(dim=0).mean().item()
+
+    return res
+
+
+def test_metrics(algorithm, loader, train_loader, device, thres=0.5):
+
+    # Get train samples
+    train_targets, _, _ = get_samples(train_loader)
 
     # preds: sigmoid output
     targets, attributes, preds, gs = predict_on_set(algorithm, loader, device) # gs: group sensitive attribute: (target, attribute) pairing?
@@ -110,24 +357,26 @@ def test_metrics(algorithm, loader, device, thres=0.5):
 
     for a in np.unique(attributes):
         mask = attributes == a
-        res['per_attribute'][int(a)] = {
+        res['per_attribute'][str(a)] = {
             **binary_metrics(targets[mask], preds_rounded[mask], label_set),
             **prob_metrics(targets[mask], preds[mask], label_set)
         }
+        res['per_attribute'][str(a)]['train_n_samples'] = len(train_targets[mask])        
 
     classes_report = classification_report(targets, preds_rounded, output_dict=True, zero_division=0.)
     res['overall']['macro_avg'] = classes_report['macro avg']
     res['overall']['weighted_avg'] = classes_report['weighted avg']
-    
+
     for y in np.unique(targets):
-        res['per_class'][int(y)] = classes_report[str(y)]
+        res['per_class'][str(y)] = classes_report[str(y)]
 
     for g in np.unique(gs):
         mask = gs == g
-        res['per_group'][g] = {
+        res['per_group'][str(g)] = {
             **binary_metrics(targets[mask], preds_rounded[mask], label_set),
             **attribute_metrics(targets[mask], preds[mask], label_set)
         }
+        res['per_group'][str(g)]['train_n_samples'] = len(train_targets[mask])
 
     res['adjusted_accuracy'] = sum([res['per_group'][g]['accuracy'] for g in np.unique(gs)]) / len(np.unique(gs))
     res['min_attr'] = pd.DataFrame(res['per_attribute']).min(axis=1).to_dict()
@@ -155,7 +404,7 @@ def eval_metrics(algorithm, loader, device, thres=0.5):
 
     for a in np.unique(attributes):
         mask = attributes == a
-        res['per_attribute'][int(a)] = {
+        res['per_attribute'][str(a)] = {
             **binary_metrics(targets[mask], preds_rounded[mask], label_set),
             **prob_metrics(targets[mask], preds[mask], label_set)
         }
@@ -165,11 +414,11 @@ def eval_metrics(algorithm, loader, device, thres=0.5):
     res['overall']['weighted_avg'] = classes_report['weighted avg']
 
     for y in np.unique(targets):
-        res['per_class'][int(y)] = classes_report[str(y)]
+        res['per_class'][str(y)] = classes_report[str(y)]
 
     for g in np.unique(gs):
         mask = gs == g
-        res['per_group'][g] = {
+        res['per_group'][str(g)] = {
             **binary_metrics(targets[mask], preds_rounded[mask], label_set),
             **attribute_metrics(targets[mask], preds[mask], label_set)
         }
@@ -232,6 +481,8 @@ def binary_metrics(targets, preds, label_set=[0, 1], return_arrays=False):
 
 
 def attribute_metrics(targets, preds, label_set, return_arrays=False):
+    
+    # breakpoint()
     if len(targets) == 0:
         return {}
 
@@ -252,6 +503,9 @@ def attribute_metrics(targets, preds, label_set, return_arrays=False):
 
 
 def prob_metrics(targets, preds, label_set, return_arrays=False):
+    
+    # breakpoint()
+    
     if len(targets) == 0:
         return {}
 
